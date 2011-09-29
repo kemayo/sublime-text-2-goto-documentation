7cd2ea58c3af340811c0443beb504dfbd48882a8
David Lynch <kemayo@gmail.com>
2011-09-29 04:02:04 -0500

    Ooops, use spaces

--- gotodocumentation.py
+++ gotodocumentation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import os
 import sublime
 import sublime_plugin
 
@@ -8,21 +7,21 @@ def open_url(url):
     sublime.active_window().run_command('open_url', {"url": url})
 
 class GotoDocumentationCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		for region in self.view.sel():
-			word = self.view.word(region)
-			if not word.empty():
-				# scope is, e.g. "text.html.basic source.php.embedded.block.html keyword.other.new.php"
-				scope = self.view.scope_name(word.begin()).strip()
-				extracted_scope = scope.rpartition('.')[2]
-				keyword = self.view.substr(word)
-				getattr(self, '%s_doc' % extracted_scope, self.unsupported)(keyword, scope)
-	
-	def unsupported(self, keyword, scope):
-		sublime.status_message("This scope is not supported: %s" % scope.rpartition('.')[2])
-	
-	def php_doc(self, keyword, scope):
-		open_url("http://php.net/%s" % keyword)
+    def run(self, edit):
+        for region in self.view.sel():
+            word = self.view.word(region)
+            if not word.empty():
+                # scope: "text.html.basic source.php.embedded.block.html keyword.other.new.php"
+                scope = self.view.scope_name(word.begin()).strip()
+                extracted_scope = scope.rpartition('.')[2]
+                keyword = self.view.substr(word)
+                getattr(self, '%s_doc' % extracted_scope, self.unsupported)(keyword, scope)
+    
+    def unsupported(self, keyword, scope):
+        sublime.status_message("This scope is not supported: %s" % scope.rpartition('.')[2])
 
-	def js_doc(self, keyword, scope):
-		open_url("https://developer.mozilla.org/en-US/search?q=%s" % keyword)
+    def php_doc(self, keyword, scope):
+        open_url("http://php.net/%s" % keyword)
+
+    def js_doc(self, keyword, scope):
+        open_url("https://developer.mozilla.org/en-US/search?q=%s" % keyword)
